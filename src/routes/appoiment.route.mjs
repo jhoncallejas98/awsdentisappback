8c88a7a9ab9025da46cad22a94cef3b090a99ee2
Jhon <jhoncallejas1998@hotmail.com>
2025-07-10 14:57:01 -0500

    actualizacion

--- src/routes/appoiment.route.mjs
+++ src/routes/appoiment.route.mjs
@@ -13,8 +13,8 @@ import { authUser } from "../middlewares/auth-user.middleware.mjs";
 const router = Router(); // Instancia del router de Express
 
 // Definición de rutas para la entidad "Appoiment"
-router.get('/api/appoiment', getAppoiment); // Obtener todas las citas
-router.post('/api/appoiment', createAppoiment); // Crear una cita
+router.get('/api/appoiment', authUser, getAppoiment); // Obtener todas las citas
+router.post('/api/appoiment', authUser, createAppoiment); // Crear una cita
 router.put('/api/appoiment', authUser, updateAllAppoiment); // Actualizar todas las citas
 router.delete('/api/appoiment', authUser, deleteAppoiment); // Eliminar todas las citas
 router.get('/api/appoiment/:id', authUser, getAppoimentById); // Obtener una cita por ID
--- src/schemas/disponibilidad.schemas.mjs
+++ src/schemas/disponibilidad.schemas.mjs
@@ -16,25 +16,24 @@ const disponibilidadSchema = new mongoose.Schema({
         type: String,                            // formato 'HH:mm'
         required: [true, "es obligatorio que se ponga la hora de inicio"]
     },
-    // horaFin: {
-    //     type: String,                            // formato 'HH:mm'
-    //     required: true
-    // },
-    // activo: {
-    //     type: Boolean,
-    //     default: true
-    // }
-    // TODO: Preguntar La funcionalidad.
-    // bloquesDisponibles: [{ 
-    //     type: String,
-    //     enum: [
-    //         '08:00', '08:20', '08:40', '09:00', '09:20', '09:40', '10:00', '10:20', '10:40',
-    //         '11:00', '11:20', '11:40', '12:00', '12:20', '12:40', '13:00', '13:20', '13:40',
-    //         '14:00', '14:20', '14:40', '15:00', '15:20', '15:40', '16:00', '16:20', '16:40',
-    //         '17:00', '17:20', '17:40', 'Por Asignar'
-    //     ], 
-    //     default: "Por Asignar"
-    // }],
+    horaFin: {
+        type: String,                            // formato 'HH:mm'
+        required: true
+    },
+    activo: {
+        type: Boolean,
+        default: true
+    },
+    bloquesDisponibles: [{ 
+        type: String,
+        enum: [
+            '08:00', '08:20', '08:40', '09:00', '09:20', '09:40', '10:00', '10:20', '10:40',
+            '11:00', '11:20', '11:40', '12:00', '12:20', '12:40', '13:00', '13:20', '13:40',
+            '14:00', '14:20', '14:40', '15:00', '15:20', '15:40', '16:00', '16:20', '16:40',
+            '17:00', '17:20', '17:40', 'Por Asignar'
+        ], 
+        default: "Por Asignar"
+    }],
     
     activo: { type: Boolean, default: true }
 }, { timestamps: true, versionKey: false });
