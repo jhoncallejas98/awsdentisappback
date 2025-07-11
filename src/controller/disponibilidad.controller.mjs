197461f590bdad746b8a2d701ad161db987eec01
Jhon <jhoncallejas1998@hotmail.com>
2025-07-11 15:35:39 -0500

    cambios dispo

--- src/controller/disponibilidad.controller.mjs
+++ src/controller/disponibilidad.controller.mjs
@@ -50,8 +50,8 @@ const getAllDisponibilidad = async (req, res) => {
         
         let diaSemanaFinal = diaSemana;
         if (date) {
-            // Convertir la fecha a día de la semana (en inglés)
-            const dias = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+            // Convertir la fecha a día de la semana (en español)
+            const dias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
             const fecha = new Date(date);
             diaSemanaFinal = dias[fecha.getDay()];
             filter.diaSemana = diaSemanaFinal;
